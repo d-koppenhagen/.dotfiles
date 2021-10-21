53c44a7aaa9d542c0200e7fb68bb44d844512eda
Danny Koppenhagen <mail@d-koppenhagen.de>
2021-10-21 09:17:11 +0200

    format time machinbe exclude command

--- time-machine-excludes.sh
+++ time-machine-excludes.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
-# exclude all `node_modules` folders within the dev directory
-find "$HOME/dev" -name 'node_modules' -prune -type d -exec tmutil addexclusion {} \; -exec tmutil isexcluded {} \;
+# exclude all `node_modules` inside the `~/dev` directory
+find "$HOME/dev" \
+  -name 'node_modules' \
+  -prune \
+  -type d \
+  -exec tmutil addexclusion {} \; \
+  -exec tmutil isexcluded {} \;
 echo "Done. The excluded files won't be part of a time machine backup anymore."
