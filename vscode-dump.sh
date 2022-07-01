5af9476da10d523f57719edfe68000fd5fdcb194
Danny Koppenhagen <mail@d-koppenhagen.de>
2022-07-01 21:54:51 +0200

    remove vscode dumps
    such configs will be synced via natice sync functionality

--- vscode-dump.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-TARGET=vscode-extensions-list.txt
-echo "dump all vscode file extension names into $TARGET"
-code --list-extensions >> $TARGET
--- vscode-install.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-TARGET=vscode-extensions-list.txt
-echo "install all vscode extension files from $TARGET"
-cat $TARGET | xargs -n 1 code --install-extension
